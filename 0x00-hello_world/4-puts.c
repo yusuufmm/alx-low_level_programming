781c816573bd5aabf2b4c2d047659093ad5c4193
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2022-09-12 16:26:51 -0700

    C level tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	puts("programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
