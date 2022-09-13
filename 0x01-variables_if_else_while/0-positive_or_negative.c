3388aa224ffe738585ba0a357d0166f1b46e2dcf
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2022-09-12 19:54:49 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,6 +12,7 @@ int main(void)
 		int n;
 
 			srand(time(0));
+
 				n = rand() - RAND_MAX / 2;
 					
 				if (n>0)
@@ -20,7 +21,7 @@ int main(void)
 					printf("%d is positive\n", n);
 
 				}
-                           	else if (n ==0)
+                           	else if (n == 0)
                                	{
 
 				        printf("%d is zero\n", n);
