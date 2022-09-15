e95f0f34807f09ff3ea25bfeb71184424652f392
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2022-09-14 19:29:03 -0700

    My nested tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,14 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_alphabet_x10 - function that will print the alphabet 10 times
- *
+ * print_alphabet 
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	int line, ch;
+	int ch;
 
-	for (line = 0; line <= 9; ++line)
-	{
-		for (ch = 'a'; ch <= 'z'; ++ch)
-			_putchar(ch);
-		_putchar('\n');
-	}
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
 }
