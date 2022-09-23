43ff67d80ecc71e5c51384c6f5a10744298b03b2
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2022-09-22 18:26:55 -0700

    tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - Prints a number
- * @n: The number to print
+ * print_number - prints an integer
+ *@n:integer to be printed
  *
  */
 void print_number(int n)
@@ -15,10 +15,9 @@ void print_number(int n)
 		num = -num;
 	}
 
-	if (num > 9)
-	{
+	if ((num / 10) > 0)
 		print_number(num / 10);
-	}
-	_putchar(num % 10 + '0');
+
+	_putchar((num % 10) + '0');
 }
 
