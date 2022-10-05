f4172ca22bcc70cffd002b78121b1edd55bf6306
YUSUF MUHAMMAD MUSA <yusuf2000mm@gmail.com>
2022-10-05 16:51:30 -0700

    malloc advanced tasks

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
 void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
